f4b2c596f5d74c258b1b82443b5985a8b8562273
teseo <sol@javiermellado.com>
2017-02-26 02:42:49 +0000

    remove wrong phpdocs

--- src/app/Middleware/CsrfMiddleware.php
+++ src/app/Middleware/CsrfMiddleware.php
@@ -3,13 +3,6 @@
  * Copyright (c) 2017. This file belongs to Misericordia di "Torre del lago Puccini"
  */
 
-/**
- * Created by PhpStorm.
- * User: javi
- * Date: 25/02/2017
- * Time: 21:57
- */
-
 namespace App\Middleware;
 
 
