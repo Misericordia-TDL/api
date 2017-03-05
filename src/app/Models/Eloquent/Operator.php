0f425dcf76adc7df65c19505ce5f3778aa0acddf
teseo <sol@javiermellado.com>
2017-03-05 13:25:16 +0000

    change operator date fields

--- src/app/Models/Eloquent/Operator.php
+++ src/app/Models/Eloquent/Operator.php
@@ -6,9 +6,23 @@
 namespace App\Models\Eloquent;
 
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
 
 class Operator extends Eloquent
 {
     protected $collection = 'operator';
+
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 
 }
