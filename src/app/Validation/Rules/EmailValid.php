d504db94cf72f9938af2b4bb4a50553683dce00d
teseo <sol@javiermellado.com>
2017-03-05 13:26:16 +0000

    update email valid custom rule

--- src/app/Validation/Rules/EmailValid.php
+++ src/app/Validation/Rules/EmailValid.php
@@ -5,7 +5,7 @@
 
 namespace App\Validation\Rules;
 
-use App\Models\Operator;
+use App\Models\Eloquent\Operator;
 use Respect\Validation\Rules\AbstractRule;
 
 class EmailValid extends AbstractRule
@@ -13,14 +13,12 @@ class EmailValid extends AbstractRule
     protected $operatorModel;
 
     function __construct(
-        Operator $operatorModel
     )
     {
-        $this->operatorModel = $operatorModel;
     }
 
     public function validate($input)
     {
-        return !empty($this->operatorModel->findByEmail($input));
+        return !empty(Operator::where('email', '=', $input)->first());
     }
 }
