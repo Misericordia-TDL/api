c317583c87756fea198280e8fbb30d37b809fa69
teseo <sol@javiermellado.com>
2017-03-07 22:47:37 +0000

    comments in dependencies file

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,39 +1,48 @@
 <?php
 /**
  * Copyright (c) 2017. This file belongs to Misericordia di "Torre del lago Puccini"
+ *
+ * In this class is where all the component dependencies of the application will be loaded
+ * whether is coming from a package imported via composer like Capsule or Eloquent
+ * or an custom application module like Operator or OperatorLevel
+ *
+ * @see https://www.slimframework.com/docs/concepts/di.html
+ * @author Javier Mellado <sol@javiermellado.com>
  */
 
-use Respect\Validation\Validator;
 use App\Core\Services\Common as CommonServices;
-use App\Operator\Services\Operator as Operator;
-use App\Operator\Services\OperatorActions;
-use App\OperatorLevel\Services\OperatorLevel;
-use App\OperatorLevel\Services\OperatorLevelActions;
-use App\Home\Services\Home;
-use App\Home\Services\HomeActions;
+use App\Home\Services\Home as HomeService;
+use App\Home\Services\HomeActions as HomeActionsService;
+use App\Operator\Services\Operator as OperatorService;
+use App\Operator\Services\OperatorActions as OperatorActionsService;
+use App\OperatorLevel\Services\OperatorLevel as OperatorLevelService;
+use App\OperatorLevel\Services\OperatorLevelActions as OperatorLevelActionsService;
 use Illuminate\Database\Capsule\Manager;
 use Jenssegers\Mongodb\Connection;
+use Respect\Validation\Validator;
+
 // DIC configuration
 $container = $app->getContainer();
 
-//ACTIONS
+//Here we load all the Validation rules that will be used in actions
 Validator::with('App\\Validation\\Rules');
 
+//Here all the dependencies get registered
 $container->register(new CommonServices());
-$container->register(new Operator());
-$container->register(new OperatorActions());
-$container->register(new OperatorLevel());
-$container->register(new OperatorLevelActions());
-$container->register(new Home());
-$container->register(new HomeActions());
+$container->register(new OperatorService());
+$container->register(new OperatorActionsService());
+$container->register(new OperatorLevelService());
+$container->register(new OperatorLevelActionsService());
+$container->register(new HomeService());
+$container->register(new HomeActionsService());
 
 //Setup eloquent for mongodb to work
 $capsule = new Manager();
-$capsule->getDatabaseManager()->extend('mongodb', function($config)
-{
+$capsule->getDatabaseManager()->extend('mongodb', function ($config) {
     return new Connection($config);
 });
 
+//setup eloquent to kick in when application runs
 $capsule->addConnection($container['settings']['db']);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
