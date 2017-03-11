8f1fce66ed359ca1ac6429b6a95b34840c54b4f4
teseo <sol@javiermellado.com>
2017-03-11 14:44:48 +0000

    phpdock in validator

--- src/app/Validation/Validator.php
+++ src/app/Validation/Validator.php
@@ -6,13 +6,23 @@
 namespace App\Validation;
 
 use Respect\Validation\Exceptions\NestedValidationException;
+use Slim\Http\Request;
 
+/**
+ * Class Validator
+ * @package App\Validation
+ */
 class Validator
 {
     /** @var  array */
     protected $errors;
 
-    public function validate($request, $rules)
+    /**
+     * @param $request
+     * @param $rules
+     * @return Validator
+     */
+    public function validate(Request $request, array $rules): Validator
     {
         foreach ($rules as $field => $rule) {
             try {
@@ -28,6 +38,9 @@ class Validator
         return $this;
     }
 
+    /**
+     * @return bool
+     */
     public function failed()
     {
         return !empty($this->errors);
