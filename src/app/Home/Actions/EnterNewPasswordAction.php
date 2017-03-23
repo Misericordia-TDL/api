7db6c9f47ff7ad9c4e9c0b46067d020405872cf3
teseo <sol@javiermellado.com>
2017-03-23 00:13:19 +0000

    enter new password action done

--- src/app/Home/Actions/EnterNewPasswordAction.php
+++ src/app/Home/Actions/EnterNewPasswordAction.php
@@ -5,9 +5,12 @@
 
 namespace App\Home\Actions;
 
+use App\Operator\Repository\OperatorRepository;
 use Psr\Http\Message\ResponseInterface;
+use Slim\Flash\Messages;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Interfaces\RouterInterface;
 use Slim\Views\Twig as View;
 
 /**
@@ -21,16 +24,37 @@ final class EnterNewPasswordAction
      * @var View
      */
     protected $view;
+    /**
+     * @var OperatorRepository
+     */
+    private $operatorRepository;
+    /**
+     * @var Messages
+     */
+    private $flash;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
 
     /**
      * IndexAction constructor.
      * @param View $view
+     * @param OperatorRepository $operatorRepository
+     * @param Messages $flash
+     * @param RouterInterface $router
      */
     function __construct(
-        View $view
+        View $view,
+        OperatorRepository $operatorRepository,
+        Messages $flash,
+        RouterInterface $router
     )
     {
         $this->view = $view;
+        $this->operatorRepository = $operatorRepository;
+        $this->flash = $flash;
+        $this->router = $router;
     }
 
     /**
@@ -40,10 +64,21 @@ final class EnterNewPasswordAction
      */
     public function __invoke(Request $request, Response $response): ResponseInterface
     {
+        try {
+            $token = $request->getAttribute('token');
 
-        $data = [
-            'token' => $request->getAttribute('token')
-        ];
-        return $this->view->render($response, 'partials/home/enter-new-password.twig', $data);
+            //we get the operator by the token
+            //if is not valid, exception will be captured
+            $operator = $this->operatorRepository->findByResetToken($token);
+
+            $data = [
+                'token' => $token,
+                'fullName' => $operator->name
+            ];
+            return $this->view->render($response, 'partials/home/enter-new-password.twig', $data);
+        } catch (\InvalidArgumentException $e) {
+            $this->flash->addMessage('error', 'The reset password link is not valid.');
+            return $response->withRedirect($this->router->pathFor('home'));
+        }
     }
 }
