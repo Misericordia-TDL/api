2ce7326150bfafba1ba81ae49508bb262c481f37
teseo <sol@javiermellado.com>
2017-05-21 17:32:49 +0100

    create abstract create action in core

---
+++ src/app/Core/Actions/CreateAction.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright (c) 2017. This file belongs to Misericordia di "Torre del lago Puccini"
+ */
+
+namespace App\Core\Actions;
+
+use App\Core\Repository\RepositoryInterface;
+use App\Validation\Validator;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Flash\Messages;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Interfaces\RouterInterface;
+
+/**
+ * Class CreateAction
+ * @package App\Core\Actions
+ * @author Javier Mellado <sol@javiermellado.com>
+ * @author Cyprian Laskowski <cyplas@gmail.com>
+ */
+abstract class CreateAction
+{
+    /**
+     * @var RepositoryInterface
+     */
+    protected $repository;
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+    /**
+     * @var Validator
+     */
+    protected $validator;
+    /**
+     * @var Messages
+     */
+    protected $flash;
+
+    /**
+     * FoodController constructor.
+     * @param RouterInterface $router
+     * @param Validator $validator
+     * @param RepositoryInterface $repository
+     * @param Messages $flash
+     */
+    function __construct(
+        RouterInterface $router,
+        Validator $validator,
+        RepositoryInterface $repository,
+        Messages $flash
+    )
+    {
+        $this->repository = $repository;
+        $this->router = $router;
+        $this->validator = $validator;
+        $this->flash = $flash;
+    }
+
+    abstract function __invoke(Request $request, Response $response): ResponseInterface;
+}
