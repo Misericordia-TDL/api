8399d182261b48e961434c52b4b6b0abb2ce094a
teseo <sol@javiermellado.com>
2017-06-19 15:26:24 +0100

    refactor delete food action

--- src/app/Food/Actions/DeleteFoodAction.php
+++ src/app/Food/Actions/DeleteFoodAction.php
@@ -7,75 +7,17 @@
 
 namespace App\Food\Actions;
 
-use App\Food\Repository\FoodRepository;
-use Psr\Http\Message\ResponseInterface;
-use Slim\Flash\Messages;
-use Slim\Http\Request;
-use Slim\Http\Response;
-use Slim\Interfaces\RouterInterface;
+use App\Core\Actions\DeleteAction;
 
 /**
  * Class DeleteFoodAction
  * @package App\Food\Actions
  * @author Cyprian Laskowski <cyplas@gmail.com>
  */
-final class DeleteFoodAction
+final class DeleteFoodAction extends DeleteAction
 {
     /**
-     * @var FoodRepository
+     * @var string
      */
-    protected $foodRepository;
-    /**
-     * @var RouterInterface
-     */
-    private $router;
-    /**
-     * @var Messages
-     */
-    private $flash;
-
-    /**
-     * FoodController constructor.
-     * @param RouterInterface $router
-     * @param FoodRepository $foodRepository
-     * @param Messages $flash
-     * @internal param Validator $validator
-     * @internal param View $view
-     */
-    function __construct(
-        RouterInterface $router,
-        FoodRepository $foodRepository,
-        Messages $flash
-    )
-    {
-        $this->router = $router;
-        $this->flash = $flash;
-        $this->foodRepository = $foodRepository;
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function __invoke(Request $request, Response $response): ResponseInterface
-    {
-
-        $id = $request->getParam('id');
-
-        //delete a food and in case of an error, flash message with error message
-        //will be sent to the view.
-        try {
-
-            if ($this->foodRepository->delete($id)) {
-                $this->flash->addMessage('info', 'Food disabled correctly');
-            } else {
-                $this->flash->addMessage('error', 'Food not disabled correctly');
-            }
-        } catch (\InvalidArgumentException $e) {
-            $this->flash->addMessage('error', 'Food not found. Could not perform deletion.');
-        }
-
-        return $response->withRedirect($this->router->pathFor('list-food'));
-    }
+    protected $element = 'food';
 }
