c5a1a098956e8f128a875892c7c7d30f704c0d1e
teseo <sol@javiermellado.com>
2017-06-19 15:41:54 +0100

    refactor edit medicine

--- src/app/Medicine/Actions/EditMedicineAction.php
+++ src/app/Medicine/Actions/EditMedicineAction.php
@@ -7,71 +7,21 @@
 
 namespace App\Medicine\Actions;
 
-use App\Medicine\Model\Medicine;
-use App\Medicine\Repository\MedicineRepository;
-use Psr\Http\Message\ResponseInterface;
-use Slim\Http\Request;
-use Slim\Http\Response;
-use Slim\Interfaces\RouterInterface;
-use Slim\Views\Twig as View;
+use App\Core\Actions\EditAction;
 
 /**
  * Class EditMedicineAction
  * @package App\Medicine\Actions
  * @author Cyprian Laskowski <cyplas@gmail.com>
  */
-final class EditMedicineAction
+final class EditMedicineAction extends EditAction
 {
     /**
-     * @var View
+     * @var string
      */
-    protected $view;
+    protected $template = 'partials/medicine/edit-medicine-data.twig';
     /**
-     * @var MedicineLevelRepository
+     * @var string
      */
-    protected $medicineLevelRepository;
-    /**
-     * @var MedicineRepository
-     */
-    private $medicineRepository;
-
-    /**
-     * IndexAction constructor.
-     * @param View $view
-     * @param MedicineRepository $medicineRepository
-     * @param RouterInterface $router
-     */
-    function __construct(
-        View $view,
-        MedicineRepository $medicineRepository,
-        RouterInterface $router
-    )
-    {
-        $this->medicineRepository = $medicineRepository;
-        $this->view = $view;
-        $this->router = $router;
-
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function __invoke(Request $request, Response $response): ResponseInterface
-    {
-        //fetch data from medicine
-        //In case of medicine not found, redirect to list medicine action
-        try {
-            $id = $request->getAttribute('id');
-            /** @var Medicine $medicine */
-            $medicine = $this->medicineRepository->findById($id);
-            $data = [
-                'medicine' => $medicine,
-            ];
-            return $this->view->render($response, 'partials/medicine/edit-medicine-data.twig', $data);
-        } catch (\InvalidArgumentException $e) {
-            return $response->withRedirect($this->router->pathFor('list-medicine'));
-        }
-    }
+    protected $element = 'medicine';
 }
