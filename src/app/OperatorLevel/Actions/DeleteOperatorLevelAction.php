cc9676ce8b67882568603d641ffca10aaecd4dee
teseo <sol@javiermellado.com>
2017-06-19 15:54:06 +0100

    refactor delete operator level action

--- src/app/OperatorLevel/Actions/DeleteOperatorLevelAction.php
+++ src/app/OperatorLevel/Actions/DeleteOperatorLevelAction.php
@@ -5,81 +5,17 @@
 
 namespace App\OperatorLevel\Actions;
 
-use App\Auth\Auth;
-use App\OperatorLevel\Repository\OperatorLevelRepository;
-use Psr\Http\Message\ResponseInterface;
-use Slim\Flash\Messages;
-use Slim\Http\Request;
-use Slim\Http\Response;
-use Slim\Interfaces\RouterInterface;
+use App\Core\Actions\DeleteAction;
 
 /**
  * Class DeleteOperatorLevelAction
  * @package App\OperatorLevel
  * @author Javier Mellado <sol@javiermellado.com>
  */
-final class DeleteOperatorLevelAction
+final class DeleteOperatorLevelAction extends DeleteAction
 {
     /**
-     * @var OperatorLevelRepository
+     * @var string
      */
-    protected $operatorLevelRepository;
-    /**
-     * @var RouterInterface
-     */
-    private $router;
-    /**
-     * @var Messages
-     */
-    private $flash;
-    /**
-     * @var Auth
-     */
-    private $auth;
-
-    /**
-     * OperatorController constructor.
-     * @param RouterInterface $router
-     * @param Auth $auth
-     * @param OperatorLevelRepository $operatorLevelRepository
-     * @param Messages $flash
-     * @internal param Validator $validator
-     * @internal param View $view
-     */
-    function __construct(
-        RouterInterface $router,
-        Auth $auth,
-        OperatorLevelRepository $operatorLevelRepository,
-        Messages $flash
-    )
-    {
-        $this->router = $router;
-        $this->flash = $flash;
-        $this->operatorLevelRepository = $operatorLevelRepository;
-        $this->auth = $auth;
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function __invoke(Request $request, Response $response): ResponseInterface
-    {
-
-        $id = $request->getParam('id');
-
-        try {
-
-            if ($this->operatorLevelRepository->delete($id)) {
-                $this->flash->addMessage('info', 'Operator disabled correctly');
-            } else {
-                $this->flash->addMessage('error', 'Operator not disabled correctly');
-            }
-        } catch (\InvalidArgumentException $e) {
-            $this->flash->addMessage('error', 'Operator not found. Could not perform deletion.');
-        }
-
-        return $response->withRedirect($this->router->pathFor('list-operator-level'));
-    }
+    protected $element = 'operator-level';
 }
