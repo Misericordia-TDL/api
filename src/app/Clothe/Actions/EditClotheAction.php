3788ea79b8599ed09032b9d882ab187c08c2737f
teseo <sol@javiermellado.com>
2017-06-19 11:05:38 +0100

    refactor edit clothe action

--- src/app/Clothe/Actions/EditClotheAction.php
+++ src/app/Clothe/Actions/EditClotheAction.php
@@ -7,71 +7,21 @@
 
 namespace App\Clothe\Actions;
 
-use App\Clothe\Model\Clothe;
-use App\Clothe\Repository\ClotheRepository;
-use Psr\Http\Message\ResponseInterface;
-use Slim\Http\Request;
-use Slim\Http\Response;
-use Slim\Interfaces\RouterInterface;
-use Slim\Views\Twig as View;
+use App\Core\Actions\EditAction;
 
 /**
  * Class EditClotheAction
  * @package App\Clothe\Actions
  * @author Cyprian Laskowski <cyplas@gmail.com>
  */
-final class EditClotheAction
+final class EditClotheAction extends EditAction
 {
     /**
-     * @var View
+     * @var string
      */
-    protected $view;
+    protected $template = 'partials/clothe/edit-clothe-data.twig';
     /**
-     * @var ClotheLevelRepository
+     * @var string
      */
-    protected $clotheLevelRepository;
-    /**
-     * @var ClotheRepository
-     */
-    private $clotheRepository;
-
-    /**
-     * IndexAction constructor.
-     * @param View $view
-     * @param ClotheRepository $clotheRepository
-     * @param RouterInterface $router
-     */
-    function __construct(
-        View $view,
-        ClotheRepository $clotheRepository,
-        RouterInterface $router
-    )
-    {
-        $this->clotheRepository = $clotheRepository;
-        $this->view = $view;
-        $this->router = $router;
-
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function __invoke(Request $request, Response $response): ResponseInterface
-    {
-        //fetch data from clothe
-        //In case of clothe not found, redirect to list clothe action
-        try {
-            $id = $request->getAttribute('id');
-            /** @var Clothe $clothe */
-            $clothe = $this->clotheRepository->findById($id);
-            $data = [
-                'clothe' => $clothe,
-            ];
-            return $this->view->render($response, 'partials/clothe/edit-clothe-data.twig', $data);
-        } catch (\InvalidArgumentException $e) {
-            return $response->withRedirect($this->router->pathFor('list-clothe'));
-        }
-    }
+    protected $element = 'clothe';
 }
