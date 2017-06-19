55da46f5c11987e28d22d4d66f1d401bed084c39
teseo <sol@javiermellado.com>
2017-06-19 15:29:35 +0100

    refactor enter food action

--- src/app/Food/Actions/EnterFoodDataAction.php
+++ src/app/Food/Actions/EnterFoodDataAction.php
@@ -7,41 +7,14 @@
 
 namespace App\Food\Actions;
 
-use Psr\Http\Message\ResponseInterface;
-use Slim\Http\Request;
-use Slim\Http\Response;
-use Slim\Views\Twig as View;
+use App\Core\Actions\EnterDataAction;
 
 /**
  * Class EnterFoodDataAction
  * @package App\Food\Actions
  * @author Cyprian Laskowski <cyplas@gmail.com>
  */
-final class EnterFoodDataAction
+final class EnterFoodDataAction extends EnterDataAction
 {
-    /**
-     * @var View
-     */
-    protected $view;
-   
-    /**
-     * FoodController constructor.
-     * @param View $view
-     */
-    function __construct(
-        View $view
-    )
-    {
-        $this->view = $view;
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function __invoke(Request $request, Response $response): ResponseInterface
-    {
-        return $this->view->render($response, 'partials/food/enter-food-data.twig', []);
-    }
+    protected $template = 'partials/food/enter-food-data.twig';
 }
